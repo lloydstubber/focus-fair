66f7b0c9aa77a2409dbaaa63c00e605bf48e664c
Lloyd Stubber <info@squareyes.info>
2017-07-21 11:19:06 +1000

    Scroll function and styles

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -3,59 +3,32 @@
 
 $(document).ready(function() {
 
-  // Function to strike out on scroll
-  $(window).on("scroll", function(){
-        if( $('body').scrollTop() > 50 && $('body').scrollTop() < 400){
-          $('h1.one').addClass('active');
+    // Function to strike out on scroll
+    $(window).on("scroll", function(){
+
+        // Distance of element from top
+        var pos1 = Math.floor($('h1.one').offset().top);
+        var pos2 = Math.floor($('h1.two').offset().top);
+        var pos3 = Math.floor($('h1.three').offset().top);
+        var pos4 = Math.floor($('h1.four').offset().top);
+
+        // Find middle of page and calc range either side
+        var middlePage = Math.floor($(document).scrollTop() + $(window).height() / 2.0);
+        var middleTop = middlePage - 10;
+        var middleBottom = middlePage + 10;
+
+        // Check element within range and add class
+        if( pos1 >= middleTop && pos1 <= middleBottom ) {
+            $('h1.one').addClass('active');
         }
-        else if( $('body').scrollTop() > 400 && $('body').scrollTop() < 700){
-          $('h1.two').addClass('active');
+        else if ( pos2 >= middleTop && pos2 <= middleBottom ) {
+            $('h1.two').addClass('active');
         }
-        else if( $('body').scrollTop() > 700 && $('body').scrollTop() < 1100){
-          $('h1.three').addClass('active');
+        else if ( pos3 >= middleTop && pos3 <= middleBottom ) {
+            $('h1.three').addClass('active');
         }
-        else if( $('body').scrollTop() > 1100 && $('body').scrollTop() < 1300){
-          $('h1.four').addClass('active');
+        else if ( pos4 >= middleTop && pos4 <= middleBottom ) {
+            $('h1.four').addClass('active');
         }
-
-      });
-
-  });
-
-
-// function spellDemTing() {
-
-//   var seeya = [
-//     " ",
-//     "S",
-//     "Se",
-//     "See",
-//     "See y",
-//     "See yo",
-//     "See you",
-//     "See you ",
-//     "See you t",
-//     "See you th",
-//     "See you the",
-//     "See you ther",
-//     "See you there",
-//     "See you there!"
-//   ];
-//           var i=0;
-//         setInterval(function(){
-//             $('h1.five').html(seeya[i]);
-//             i++;
-//           }, 100);
-// };
-
-// $(window).scroll(function() {
-//    var hT = $('h1.five').offset().top,
-//        hH = $('h1.five').outerHeight(),
-//        wH = $(window).height(),
-//        wS = $(this).scrollTop();
-
-//    if ( wS > (hT+hH-wH) ){
-//     spellDemTing();
-//    }
-
-// });
+    });
+});
